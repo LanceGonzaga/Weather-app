8578a88b4c256cb6e6e4b1c4eaf2580ddf13d23c
LanceGonzaga <lancegonzaga753@gmail.com>
2025-03-22 05:57:48 +0800

    Update Weather.jsx

--- Weather app/src/components/Weather.jsx
+++ Weather app/src/components/Weather.jsx
@@ -69,7 +69,7 @@ const Weather = () => {
   return (
     <div className='weather'>
       <div className="search-bar">
-        <input ref={inputRef} type="text" placeholder='Search a City'/>
+        <input ref={inputRef} type="text" placeholder='Search'/>
         <img src={search_icon} alt=""onClick={()=>search(inputRef.current.value)}/>
       </div>
       {weatherData?<>
